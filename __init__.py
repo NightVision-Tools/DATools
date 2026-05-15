f4c097743d016b60c0cf11da77bd940054876c19
NightVision <LuigiNightVision@gmail.com>
2026-05-15 15:18:33 +0200

    Added a basic main panel for starter

--- __init__.py
+++ __init__.py
@@ -1,10 +1,36 @@
-
-
 """Init DATools"""
 
+bl_info = {
+    "name": "DATools",
+    "author": "DATools",
+    "version": (1, 0, 0),
+    "blender": (5, 1, 0),
+    "location": "View3D > Sidebar > DAT",
+    "description": "A set of tools for the DungeonAlchemist™ import pipeline",
+    "category": "3D View",
+}
+
 import bpy
-import addon_utils
-import sys
-import os
-import importlib
-import json
+
+from .ui.main_panel import DAT_3DV_MainPanel, register_scene_properties
+
+# register classes
+classes = (
+    DAT_3DV_MainPanel,
+)
+
+
+def register():
+    register_scene_properties()
+
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+
+def unregister():
+    for cls in reversed(classes):
+        bpy.utils.unregister_class(cls)
+
+    if hasattr(bpy.types.Scene, "ui_menu"):
+        delattr(bpy.types.Scene, "ui_menu")
+
---
+++ ui/main_panel.py
@@ -0,0 +1,41 @@
+import bpy
+
+
+def ui_menu_options(self, context):
+    enum_items = [
+        ("Blender", "Blender", "Blender", "BLENDER", 1),
+        ("Tools", "Tools", "Tools", "TOOL_SETTINGS", 2),
+        ("I/O", "I/O", "I/O", "NETWORK_DRIVE", 4),
+        ("Settings", "Settings", "Settings", "SETTINGS", 8),
+    ]
+    return enum_items
+
+
+class DAT_3DV_MainPanel(bpy.types.Panel):
+    bl_label = "DATools"
+    bl_idname = "DAT_3DV_MainPanel"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "DAT"
+    bl_order = 0
+
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+        row = layout.row()
+        row.prop(scene, "ui_menu", text="")
+
+        menu = set(scene.ui_menu)
+        if not menu:
+            layout.label(text="SELECT A MENU!", icon="ERROR")
+            return
+        
+        layout.label(text=f"Selected: {', '.join(sorted(menu))}")
+
+
+def register_scene_properties():
+    bpy.types.Scene.ui_menu = bpy.props.EnumProperty(
+        name="UI Menu",
+        items=ui_menu_options,
+        options={"ENUM_FLAG"},
+    )
